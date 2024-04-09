205b703a2c811e2ef5a8db150311c93077801932
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-04-10 01:00:56 +0530

    BookStore Project  - Frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
