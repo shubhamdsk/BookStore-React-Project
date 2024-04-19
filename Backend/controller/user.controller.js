1110140068bede481c66849e893b7786b93e3cad
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-04-19 13:27:20 +0530

    BookStore - Backend : Login Model Updated with strict verification with all the possibilities

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -22,17 +22,23 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid Email " });
-    }
-    const isMatch = await bcryptjs.compare(password, user.password);
-    if (!isMatch) {
+    const isMatch = user
+      ? await bcryptjs.compare(password, user.password)
+      : false;
+
+    if (!user && !isMatch) {
+      return res.status(400).json({ message: "Invalid Email and Password" });
+    } else if (!user) {
+      return res.status(400).json({ message: "Invalid Email" });
+    } else if (!isMatch) {
       return res.status(400).json({ message: "Invalid Password" });
     }
+
     return res.status(200).json({
       message: "User logged in successfully",
       user: {
