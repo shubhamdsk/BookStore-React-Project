1cce899d8621644b83fd26deb504b4c0274c8b61
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-04-21 00:37:44 +0530

    BookStore  - Project Completed

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
+import toast from "react-hot-toast";
 function Login() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
 
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:1157/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Loggedin Successfully");
+          document.getElementById("my_modal_3").close();
+          setTimeout(() => {
+            window.location.reload();
+            localStorage.setItem("Users", JSON.stringify(res.data.user));
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+          setTimeout(() => {}, 2000);
+        }
+      });
+  };
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
@@ -27,7 +53,7 @@ function Login() {
             <h3 className="font-bold text-lg">Login</h3>
             {/* Email */}
             <div className="mt-4 space-y-2">
-              <label>Email</label> {/* Added label for accessibility */}
+              <span>Email</span>
               <br />
               <input
                 type="email"
@@ -44,7 +70,7 @@ function Login() {
             </div>
             {/* password */}
             <div className="mt-4 space-y-2">
-              <label>Password</label> {/* Added label for accessibility */}
+              <span>Password</span>
               <br />
               <input
                 type="password"
