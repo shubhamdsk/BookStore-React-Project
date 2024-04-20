b66bbcf8643d58096ca82cbbf109038aea430260
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-04-21 00:36:17 +0530

    BookStore- Login and Signup beckend completed

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -16,7 +16,14 @@ export const signup = async (req, res) => {
       password: hashPassword,
     });
     await createdUser.save();
-    return res.status(201).json({ message: "User created successfully" });
+    return res.status(201).json({
+      message: "User created successfully",
+      user: {
+        _id: createdUser._id,
+        fullname: createdUser.fullname,
+        email: createdUser.email,
+      },
+    });
   } catch (error) {
     console.log("Error: " + error.message);
     return res.status(500).json({ message: "Internal server error" });
--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 - For backend applications we have used the Express Js and Node Js components.
 - For Database we have used the Mongo DB Database
 - For API Testing we whave the Postman
+
+# Images related to the Proeject
+
+- ![6888606](https://github.com/shubhamdsk/BookStore-React-Project/assets/87111500/17fa458a-e09e-464f-ab05-c962ab208641)
+
+- ![6920933](https://github.com/shubhamdsk/BookStore-React-Project/assets/87111500/812e9ce9-3180-4685-bd7b-c0edc80a1bef)
